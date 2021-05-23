e6d17f3dfe02e054e91806692e9d3b0f6076557e
compiler13 <compiler13@gitlab.com>
2021-05-23 21:10:44 +0800

    PR002: modify listener according to g4

--- src/semanticAnalysis.cpp
+++ src/semanticAnalysis.cpp
@@ -1,9 +1,17 @@
 #include "semanticAnalysis.h"
 
-void SemanticAnalysis::enterConstDecl(CACTParser::ConstDeclContext * ctx)
-{}
-void SemanticAnalysis::exitConstDecl(CACTParser::ConstDeclContext * ctx)
-{
+void SemanticAnalysis::enterCompUnit(CACTParser::CompUnitContext * ctx) {
+    
+}
+void SemanticAnalysis::exitCompUnit(CACTParser::CompUnitContext * ctx) {
+    
+}
+
+void SemanticAnalysis::enterDecl(CACTParser::DeclContext * ctx) {}
+void SemanticAnalysis::exitDecl(CACTParser::DeclContext * ctx) {}
+
+void SemanticAnalysis::enterConstDecl(CACTParser::ConstDeclContext * ctx) {}
+void SemanticAnalysis::exitConstDecl(CACTParser::ConstDeclContext * ctx) {
     std::cout << "const variable define: " << std::endl;
     for(const auto & const_def : ctx->constDef())
     {
@@ -12,10 +20,17 @@ void SemanticAnalysis::exitConstDecl(CACTParser::ConstDeclContext * ctx)
     }
 }
 
-void SemanticAnalysis::enterVarDecl(CACTParser::VarDeclContext * ctx)
-{}
-void SemanticAnalysis::exitVarDecl(CACTParser::VarDeclContext * ctx)
-{
+void SemanticAnalysis::enterBType(CACTParser::BTypeContext * ctx) {}
+void SemanticAnalysis::exitBType(CACTParser::BTypeContext * ctx) {}
+
+void SemanticAnalysis::enterConstDef(CACTParser::ConstDefContext * ctx) {}
+void SemanticAnalysis::exitConstDef(CACTParser::ConstDefContext * ctx) {}
+
+void SemanticAnalysis::enterConstInitVal(CACTParser::ConstInitValContext * ctx) {}
+void SemanticAnalysis::exitConstInitVal(CACTParser::ConstInitValContext * ctx) {}
+
+void SemanticAnalysis::enterVarDecl(CACTParser::VarDeclContext * ctx) {}
+void SemanticAnalysis::exitVarDecl(CACTParser::VarDeclContext * ctx) {
     std::cout << "variable define: " << std::endl;
     for(const auto & var_def : ctx->varDef())
     {
@@ -24,8 +39,81 @@ void SemanticAnalysis::exitVarDecl(CACTParser::VarDeclContext * ctx)
     }
 }
 
-void SemanticAnalysis::exitConstExpNumber(CACTParser::ConstExpNumberContext * ctx)
-{
+void SemanticAnalysis::enterVarDef(CACTParser::VarDefContext * ctx) {}
+void SemanticAnalysis::exitVarDef(CACTParser::VarDefContext * ctx) {}
+
+void SemanticAnalysis::enterFuncDef(CACTParser::FuncDefContext * ctx) {}
+void SemanticAnalysis::exitFuncDef(CACTParser::FuncDefContext * ctx) {}
+
+void SemanticAnalysis::enterFuncType(CACTParser::FuncTypeContext * ctx) {}
+void SemanticAnalysis::exitFuncType(CACTParser::FuncTypeContext * ctx) {}
+
+void SemanticAnalysis::enterFuncFParams(CACTParser::FuncFParamsContext * ctx) {}
+void SemanticAnalysis::exitFuncFParams(CACTParser::FuncFParamsContext * ctx) {}
+
+void SemanticAnalysis::enterFuncFParam(CACTParser::FuncFParamContext * ctx) {}
+void SemanticAnalysis::exitFuncFParam(CACTParser::FuncFParamContext * ctx) {}
+
+void SemanticAnalysis::enterBlock(CACTParser::BlockContext * ctx) {}
+void SemanticAnalysis::exitBlock(CACTParser::BlockContext * ctx) {}
+
+void SemanticAnalysis::enterBlockItem(CACTParser::BlockItemContext * ctx) {}
+void SemanticAnalysis::exitBlockItem(CACTParser::BlockItemContext * ctx) {}
+
+void SemanticAnalysis::enterStmt(CACTParser::StmtContext * ctx) {}
+void SemanticAnalysis::exitStmt(CACTParser::StmtContext * ctx) {}
+
+void SemanticAnalysis::enterExp(CACTParser::ExpContext * ctx) {}
+void SemanticAnalysis::exitExp(CACTParser::ExpContext * ctx) {}
+
+void SemanticAnalysis::enterCond(CACTParser::CondContext * ctx) {}
+void SemanticAnalysis::exitCond(CACTParser::CondContext * ctx) {}
+
+void SemanticAnalysis::enterLVal(CACTParser::LValContext * ctx) {}
+void SemanticAnalysis::exitLVal(CACTParser::LValContext * ctx) {}
+
+void SemanticAnalysis::enterPrimaryExp(CACTParser::PrimaryExpContext * ctx) {}
+void SemanticAnalysis::exitPrimaryExp(CACTParser::PrimaryExpContext * ctx) {}
+
+void SemanticAnalysis::enterUnaryExp(CACTParser::UnaryExpContext * ctx) {}
+void SemanticAnalysis::exitUnaryExp(CACTParser::UnaryExpContext * ctx) {}
+
+void SemanticAnalysis::enterUnaryOp(CACTParser::UnaryOpContext * ctx) {}
+void SemanticAnalysis::exitUnaryOp(CACTParser::UnaryOpContext * ctx) {}
+
+void SemanticAnalysis::enterFuncRParams(CACTParser::FuncRParamsContext * ctx) {}
+void SemanticAnalysis::exitFuncRParams(CACTParser::FuncRParamsContext * ctx) {}
+
+void SemanticAnalysis::enterMulExp(CACTParser::MulExpContext * ctx) {}
+void SemanticAnalysis::exitMulExp(CACTParser::MulExpContext * ctx) {}
+
+void SemanticAnalysis::enterAddExp(CACTParser::AddExpContext * ctx) {}
+void SemanticAnalysis::exitAddExp(CACTParser::AddExpContext * ctx) {}
+
+void SemanticAnalysis::enterRelExp(CACTParser::RelExpContext * ctx) {}
+void SemanticAnalysis::exitRelExp(CACTParser::RelExpContext * ctx) {}
+
+void SemanticAnalysis::enterEqExp(CACTParser::EqExpContext * ctx) {}
+void SemanticAnalysis::exitEqExp(CACTParser::EqExpContext * ctx) {}
+
+void SemanticAnalysis::enterLAndExp(CACTParser::LAndExpContext * ctx) {}
+void SemanticAnalysis::exitLAndExp(CACTParser::LAndExpContext * ctx) {}
+
+void SemanticAnalysis::enterLOrExp(CACTParser::LOrExpContext * ctx) {}
+void SemanticAnalysis::exitLOrExp(CACTParser::LOrExpContext * ctx) {}
+
+void SemanticAnalysis::enterConstExpNumber(CACTParser::ConstExpNumberContext * ctx) {}
+void SemanticAnalysis::exitConstExpNumber(CACTParser::ConstExpNumberContext * ctx) {
     ctx->basic_or_array_and_type = ctx->number()->basic_or_array_and_type;
 }
 
+void SemanticAnalysis::enterConstExpBoolConst(CACTParser::ConstExpBoolConstContext * ctx) {}
+void SemanticAnalysis::exitConstExpBoolConst(CACTParser::ConstExpBoolConstContext * ctx) {}
+
+void SemanticAnalysis::enterNumber(CACTParser::NumberContext * ctx) {}
+void SemanticAnalysis::exitNumber(CACTParser::NumberContext * ctx) {}
+
+void SemanticAnalysis::enterEveryRule(antlr4::ParserRuleContext * ctx) {}
+void SemanticAnalysis::exitEveryRule(antlr4::ParserRuleContext * ctx) {}
+void SemanticAnalysis::visitTerminal(antlr4::tree::TerminalNode * node) {}
+void SemanticAnalysis::visitErrorNode(antlr4::tree::ErrorNode * node) {}
--- src/semanticAnalysis.h
+++ src/semanticAnalysis.h
@@ -1,4 +1,4 @@
-#pragma
+#pragma once
 
 #include "../grammar/CACTBaseListener.h"
 
@@ -11,42 +11,102 @@ public:
     ~SemanticAnalysis(){}
 
 
-    void enterCompUnit(CACTParser::CompUnitContext * /*ctx*/) override { }
-    void exitCompUnit(CACTParser::CompUnitContext * /*ctx*/) override { }
+    void enterCompUnit(CACTParser::CompUnitContext * ctx) override ;
+    void exitCompUnit(CACTParser::CompUnitContext * ctx) override ;
 
-    void enterDecl(CACTParser::DeclContext * /*ctx*/) override { }
-    void exitDecl(CACTParser::DeclContext * /*ctx*/) override { }
+    void enterDecl(CACTParser::DeclContext * ctx) override ;
+    void exitDecl(CACTParser::DeclContext * ctx) override ;
 
-    void enterConstDecl(CACTParser::ConstDeclContext * /*ctx*/) override;
-    void exitConstDecl(CACTParser::ConstDeclContext * /*ctx*/) override;
+    void enterConstDecl(CACTParser::ConstDeclContext * ctx) override;
+    void exitConstDecl(CACTParser::ConstDeclContext * ctx) override;
 
-    void enterBType(CACTParser::BTypeContext * /*ctx*/) override { }
-    void exitBType(CACTParser::BTypeContext * /*ctx*/) override { }
+    void enterBType(CACTParser::BTypeContext * ctx) override ;
+    void exitBType(CACTParser::BTypeContext * ctx) override ;
 
-    void enterConstDef(CACTParser::ConstDefContext * /*ctx*/) override { }
-    void exitConstDef(CACTParser::ConstDefContext * /*ctx*/) override { }
+    void enterConstDef(CACTParser::ConstDefContext * ctx) override ;
+    void exitConstDef(CACTParser::ConstDefContext * ctx) override ;
 
-    void enterConstInitVal(CACTParser::ConstInitValContext * /*ctx*/) override { }
-    void exitConstInitVal(CACTParser::ConstInitValContext * /*ctx*/) override { }
+    void enterConstInitVal(CACTParser::ConstInitValContext * ctx) override ;
+    void exitConstInitVal(CACTParser::ConstInitValContext * ctx) override ;
 
-    void enterVarDecl(CACTParser::VarDeclContext * /*ctx*/) override;
-    void exitVarDecl(CACTParser::VarDeclContext * /*ctx*/) override;
+    void enterVarDecl(CACTParser::VarDeclContext * ctx) override;
+    void exitVarDecl(CACTParser::VarDeclContext * ctx) override;
 
-    void enterVarDef(CACTParser::VarDefContext * /*ctx*/) override { }
-    void exitVarDef(CACTParser::VarDefContext * /*ctx*/) override { }
+    void enterVarDef(CACTParser::VarDefContext * ctx) override ;
+    void exitVarDef(CACTParser::VarDefContext * ctx) override ;
 
-    void enterConstExpNumber(CACTParser::ConstExpNumberContext * /*ctx*/) override { }
-    void exitConstExpNumber(CACTParser::ConstExpNumberContext * /*ctx*/) override;
+    void enterFuncDef(CACTParser::FuncDefContext * ctx) override ;
+    void exitFuncDef(CACTParser::FuncDefContext * ctx) override ;
 
-    void enterConstExpBoolConst(CACTParser::ConstExpBoolConstContext * /*ctx*/) override { }
-    void exitConstExpBoolConst(CACTParser::ConstExpBoolConstContext * /*ctx*/) override { }
+    void enterFuncType(CACTParser::FuncTypeContext * ctx) override ;
+    void exitFuncType(CACTParser::FuncTypeContext * ctx) override ;
 
-    void enterNumber(CACTParser::NumberContext * /*ctx*/) override { }
-    void exitNumber(CACTParser::NumberContext * /*ctx*/) override { }
+    void enterFuncFParams(CACTParser::FuncFParamsContext * ctx) override ;
+    void exitFuncFParams(CACTParser::FuncFParamsContext * ctx) override ;
+
+    void enterFuncFParam(CACTParser::FuncFParamContext * ctx) override ;
+    void exitFuncFParam(CACTParser::FuncFParamContext * ctx) override ;
+
+    void enterBlock(CACTParser::BlockContext * ctx) override ;
+    void exitBlock(CACTParser::BlockContext * ctx) override ;
+
+    void enterBlockItem(CACTParser::BlockItemContext * ctx) override ;
+    void exitBlockItem(CACTParser::BlockItemContext * ctx) override ;
+
+    void enterStmt(CACTParser::StmtContext * ctx) override ;
+    void exitStmt(CACTParser::StmtContext * ctx) override ;
+
+    void enterExp(CACTParser::ExpContext * ctx) override ;
+    void exitExp(CACTParser::ExpContext * ctx) override ;
+
+    void enterCond(CACTParser::CondContext * ctx) override ;
+    void exitCond(CACTParser::CondContext * ctx) override ;
+
+    void enterLVal(CACTParser::LValContext * ctx) override ;
+    void exitLVal(CACTParser::LValContext * ctx) override ;
+
+    void enterPrimaryExp(CACTParser::PrimaryExpContext * ctx) override ;
+    void exitPrimaryExp(CACTParser::PrimaryExpContext * ctx) override ;
+
+    void enterUnaryExp(CACTParser::UnaryExpContext * ctx) override ;
+    void exitUnaryExp(CACTParser::UnaryExpContext * ctx) override ;
+
+    void enterUnaryOp(CACTParser::UnaryOpContext * ctx) override ;
+    void exitUnaryOp(CACTParser::UnaryOpContext * ctx) override ;
+
+    void enterFuncRParams(CACTParser::FuncRParamsContext * ctx) override ;
+    void exitFuncRParams(CACTParser::FuncRParamsContext * ctx) override ;
+
+    void enterMulExp(CACTParser::MulExpContext * ctx) override ;
+    void exitMulExp(CACTParser::MulExpContext * ctx) override ;
+
+    void enterAddExp(CACTParser::AddExpContext * ctx) override ;
+    void exitAddExp(CACTParser::AddExpContext * ctx) override ;
+
+    void enterRelExp(CACTParser::RelExpContext * ctx) override ;
+    void exitRelExp(CACTParser::RelExpContext * ctx) override ;
+
+    void enterEqExp(CACTParser::EqExpContext * ctx) override ;
+    void exitEqExp(CACTParser::EqExpContext * ctx) override ;
+
+    void enterLAndExp(CACTParser::LAndExpContext * ctx) override ;
+    void exitLAndExp(CACTParser::LAndExpContext * ctx) override ;
+
+    void enterLOrExp(CACTParser::LOrExpContext * ctx) override ;
+    void exitLOrExp(CACTParser::LOrExpContext * ctx) override ;
+
+    void enterConstExpNumber(CACTParser::ConstExpNumberContext * ctx) override ;
+    void exitConstExpNumber(CACTParser::ConstExpNumberContext * ctx) override ;
+
+    void enterConstExpBoolConst(CACTParser::ConstExpBoolConstContext * ctx) override ;
+    void exitConstExpBoolConst(CACTParser::ConstExpBoolConstContext * ctx) override ;
+
+    void enterNumber(CACTParser::NumberContext * ctx) override ;
+    void exitNumber(CACTParser::NumberContext * ctx) override ;
 
 
-    void enterEveryRule(antlr4::ParserRuleContext * /*ctx*/) override { }
-    void exitEveryRule(antlr4::ParserRuleContext * /*ctx*/) override { }
-    void visitTerminal(antlr4::tree::TerminalNode * /*node*/) override { }
-    void visitErrorNode(antlr4::tree::ErrorNode * /*node*/) override { }
+    void enterEveryRule(antlr4::ParserRuleContext * ctx) override ;
+    void exitEveryRule(antlr4::ParserRuleContext * ctx) override ;
+    void visitTerminal(antlr4::tree::TerminalNode * node) override ;
+    void visitErrorNode(antlr4::tree::ErrorNode * node) override ;
 };
