1772e57f269c9e6eb75a4893a11fbc5adba0d155
yan-wenkai <yanwenkai18@mails.ucas.ac.cn>
2021-07-28 10:22:27 +0800

    PR003: update some data types in IR

--- src/IRCode.cpp
+++ src/IRCode.cpp
@@ -25,8 +25,14 @@ IROperand * IRCode::getResult() {
 IRLabelHere::IRLabelHere(IROperand * new_arg1)
 : IRCode(LABEL_HERE, nullptr, new_arg1, nullptr) { }
 
-IRParam::IRParam(IROperand * new_arg1)
-: IRCode(PARAM, nullptr, new_arg1, nullptr) { }
+IRParamW::IRParamW(IROperand * new_arg1)
+: IRCode(PARAM_W, nullptr, new_arg1, nullptr) { }
+
+IRParamF::IRParamF(IROperand * new_arg1)
+: IRCode(PARAM_F, nullptr, new_arg1, nullptr) { }
+
+IRParamD::IRParamD(IROperand * new_arg1)
+: IRCode(PARAM_D, nullptr, new_arg1, nullptr) { }
 
 IRCall::IRCall(IROperand * new_result, IROperand * new_arg1)
 : IRCode(CALL, new_result, new_arg1, nullptr) { }
@@ -34,11 +40,20 @@ IRCall::IRCall(IROperand * new_result, IROperand * new_arg1)
 IRCall::IRCall(IROperand * new_arg1)
 : IRCode(CALL, nullptr, new_arg1, nullptr) { }
 
-IRReturn::IRReturn(IROperand * new_arg1)
-: IRCode(RETURN, nullptr, new_arg1, nullptr) { }
+IRReturnW::IRReturnW(IROperand * new_arg1)
+: IRCode(RETURN_W, nullptr, new_arg1, nullptr) { }
+
+IRReturnF::IRReturnF(IROperand * new_arg1)
+: IRCode(RETURN_F, nullptr, new_arg1, nullptr) { }
+
+IRReturnD::IRReturnD(IROperand * new_arg1)
+: IRCode(RETURN_D, nullptr, new_arg1, nullptr) { }
 
 IRCopyW::IRCopyW(IROperand * new_result, IROperand * new_arg1)
 : IRCode(COPY_W, new_result, new_arg1, nullptr) { }
+
+IRCopyF::IRCopyF(IROperand * new_result, IROperand * new_arg1)
+: IRCode(COPY_F, new_result, new_arg1, nullptr) { }
 
 IRCopyD::IRCopyD(IROperand * new_result, IROperand * new_arg1)
 : IRCode(COPY_D, new_result, new_arg1, nullptr) { }
@@ -46,11 +61,17 @@ IRCopyD::IRCopyD(IROperand * new_result, IROperand * new_arg1)
 IRCopyFromIndexedW::IRCopyFromIndexedW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
 : IRCode(COPY_FROM_INDEXED_W, new_result, new_arg1, new_arg2) { }
 
+IRCopyFromIndexedF::IRCopyFromIndexedF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(COPY_FROM_INDEXED_F, new_result, new_arg1, new_arg2) { }
+
 IRCopyFromIndexedD::IRCopyFromIndexedD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
 : IRCode(COPY_FROM_INDEXED_D, new_result, new_arg1, new_arg2) { }
 
 IRCopyToIndexedW::IRCopyToIndexedW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
 : IRCode(COPY_TO_INDEXED_W, new_result, new_arg1, new_arg2) { }
+
+IRCopyToIndexedF::IRCopyToIndexedF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(COPY_TO_INDEXED_F, new_result, new_arg1, new_arg2) { }
 
 IRCopyToIndexedD::IRCopyToIndexedD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
 : IRCode(COPY_TO_INDEXED_D, new_result, new_arg1, new_arg2) { }
@@ -58,17 +79,47 @@ IRCopyToIndexedD::IRCopyToIndexedD(IROperand * new_result, IROperand * new_arg1,
 IRAddInt::IRAddInt(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
 : IRCode(ADD_INT, new_result, new_arg1, new_arg2) { }
 
+IRAddFloat::IRAddFloat(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(ADD_FLOAT, new_result, new_arg1, new_arg2) { }
+
+IRAddDouble::IRAddDouble(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(ADD_DOUBEL, new_result, new_arg1, new_arg2) { }
+
 IRSubInt::IRSubInt(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
 : IRCode(SUB_INT, new_result, new_arg1, new_arg2) { }
+
+IRSubFloat::IRSubFloat(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(SUB_FLOAT, new_result, new_arg1, new_arg2) { }
+
+IRSubDouble::IRSubDouble(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(SUB_DOUBEL, new_result, new_arg1, new_arg2) { }
 
 IRNegInt::IRNegInt(IROperand * new_result, IROperand * new_arg1)
 : IRCode(NEG_INT, new_result, new_arg1, nullptr) { }
 
+IRNegFloat::IRNegFloat(IROperand * new_result, IROperand * new_arg1)
+: IRCode(NEG_FLOAT, new_result, new_arg1, nullptr) { }
+
+IRNegDouble::IRNegDouble(IROperand * new_result, IROperand * new_arg1)
+: IRCode(NEG_DOUBLE, new_result, new_arg1, nullptr) { }
+
 IRMulInt::IRMulInt(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
 : IRCode(MUL_INT, new_result, new_arg1, new_arg2) { }
 
+IRMulFloat::IRMulFloat(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(MUL_FLOAT, new_result, new_arg1, new_arg2) { }
+
+IRMulDouble::IRMulDouble(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(MUL_DOUBLE, new_result, new_arg1, new_arg2) { }
+
 IRDivInt::IRDivInt(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
 : IRCode(DIV_INT, new_result, new_arg1, new_arg2) { }
+
+IRDivFloat::IRDivFloat(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(DIV_FLOAT, new_result, new_arg1, new_arg2) { }
+
+IRDivDouble::IRDivDouble(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(DIV_DOUBLE, new_result, new_arg1, new_arg2) { }
 
 IRModInt::IRModInt(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
 : IRCode(DIV_INT, new_result, new_arg1, new_arg2) { }
@@ -76,23 +127,55 @@ IRModInt::IRModInt(IROperand * new_result, IROperand * new_arg1, IROperand * new
 IRIfGreaterThanZeroGoto::IRIfGreaterThanZeroGoto(IROperand * new_result, IROperand * new_arg1)
 : IRCode(IF_GREATER_THAN_ZERO_GOTO, new_result, new_arg1, nullptr) { }
 
-IRIfGreaterThanGoto::IRIfGreaterThanGoto(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
-: IRCode(IF_GREATER_THAN_GOTO, new_result, new_arg1, new_arg2) { }
+IRIfGreaterThanGotoW::IRIfGreaterThanGotoW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_GREATER_THAN_GOTO_W, new_result, new_arg1, new_arg2) { }
 
-IRIfGreaterEqualThanGoto::IRIfGreaterEqualThanGoto(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
-: IRCode(IF_GREATER_EQUAL_THAN_GOTO, new_result, new_arg1, new_arg2) { }
+IRIfGreaterThanGotoF::IRIfGreaterThanGotoF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_GREATER_THAN_GOTO_F, new_result, new_arg1, new_arg2) { }
 
-IRIfLessThanGoto::IRIfLessThanGoto(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
-: IRCode(IF_LESS_THAN_GOTO, new_result, new_arg1, new_arg2) { }
+IRIfGreaterThanGotoD::IRIfGreaterThanGotoD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_GREATER_THAN_GOTO_D, new_result, new_arg1, new_arg2) { }
 
-IRIfLessEqualThanGoto::IRIfLessEqualThanGoto(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
-: IRCode(IF_LESS_EQUAL_THAN_GOTO, new_result, new_arg1, new_arg2) { }
+IRIfGreaterEqualThanGotoW::IRIfGreaterEqualThanGotoW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_GREATER_EQUAL_THAN_GOTO_W, new_result, new_arg1, new_arg2) { }
+
+IRIfGreaterEqualThanGotoF::IRIfGreaterEqualThanGotoF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_GREATER_EQUAL_THAN_GOTO_F, new_result, new_arg1, new_arg2) { }
+
+IRIfGreaterEqualThanGotoD::IRIfGreaterEqualThanGotoD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_GREATER_EQUAL_THAN_GOTO_D, new_result, new_arg1, new_arg2) { }
+
+IRIfLessThanGotoW::IRIfLessThanGotoW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_LESS_THAN_GOTO_W, new_result, new_arg1, new_arg2) { }
+
+IRIfLessThanGotoF::IRIfLessThanGotoF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_LESS_THAN_GOTO_F, new_result, new_arg1, new_arg2) { }
+
+IRIfLessThanGotoD::IRIfLessThanGotoD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_LESS_THAN_GOTO_D, new_result, new_arg1, new_arg2) { }
+
+IRIfLessEqualThanGotoW::IRIfLessEqualThanGotoW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_LESS_EQUAL_THAN_GOTO_W, new_result, new_arg1, new_arg2) { }
+
+IRIfLessEqualThanGotoF::IRIfLessEqualThanGotoF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_LESS_EQUAL_THAN_GOTO_F, new_result, new_arg1, new_arg2) { }
+
+IRIfLessEqualThanGotoD::IRIfLessEqualThanGotoD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2)
+: IRCode(IF_LESS_EQUAL_THAN_GOTO_D, new_result, new_arg1, new_arg2) { }
 
 void IRLabelHere::print() {
     std::cout << getArg1()->getName() << ":" << std::endl;
 }
 
-void IRParam::print() {
+void IRParamW::print() {
+    std::cout << "param " << getArg1()->getName() << ";" << std::endl;
+}
+
+void IRParamF::print() {
+    std::cout << "param " << getArg1()->getName() << ";" << std::endl;
+}
+
+void IRParamD::print() {
     std::cout << "param " << getArg1()->getName() << ";" << std::endl;
 }
 
@@ -106,11 +189,24 @@ void IRCall::print() {
     }
 }
 
-void IRReturn::print() {
+void IRReturnW::print() {
+    std::cout << "return " << getArg1()->getName() << ";" << std::endl;
+}
+
+void IRReturnF::print() {
+    std::cout << "return " << getArg1()->getName() << ";" << std::endl;
+}
+
+void IRReturnD::print() {
     std::cout << "return " << getArg1()->getName() << ";" << std::endl;
 }
 
 void IRCopyW::print() {
+    std::cout << getResult()->getName() << " := "
+              << getArg1()->getName() << ";" << std::endl;
+}
+
+void IRCopyF::print() {
     std::cout << getResult()->getName() << " := "
               << getArg1()->getName() << ";" << std::endl;
 }
@@ -121,6 +217,13 @@ void IRCopyD::print() {
 }
 
 void IRCopyFromIndexedW::print() {
+    std::cout << getResult()->getName() << " := "
+              << getArg1()->getName() << " [ " 
+              << getArg2()->getName() << " ] "
+              << ";" << std::endl;
+}
+
+void IRCopyFromIndexedF::print() {
     std::cout << getResult()->getName() << " := "
               << getArg1()->getName() << " [ " 
               << getArg2()->getName() << " ] "
@@ -141,6 +244,13 @@ void IRCopyToIndexedW::print() {
               << getArg1()->getName() << ";" << std::endl;
 }
 
+void IRCopyToIndexedF::print() {
+    std::cout << getResult()->getName() << " [ "
+              << getArg2()->getName() << " ] " 
+              << " := "
+              << getArg1()->getName() << ";" << std::endl;
+}
+
 void IRCopyToIndexedD::print() {
     std::cout << getResult()->getName() << " [ "
               << getArg2()->getName() << " ] " 
@@ -154,7 +264,31 @@ void IRAddInt::print() {
               << getArg2()->getName() << ";" << std::endl;
 }
 
+void IRAddFloat::print() {
+    std::cout << getResult()->getName() << " := "
+              << getArg1()->getName() << " + "
+              << getArg2()->getName() << ";" << std::endl;
+}
+
+void IRAddDouble::print() {
+    std::cout << getResult()->getName() << " := "
+              << getArg1()->getName() << " + "
+              << getArg2()->getName() << ";" << std::endl;
+}
+
 void IRSubInt::print() {
+    std::cout << getResult()->getName() << " := "
+              << getArg1()->getName() << " - "
+              << getArg2()->getName() << ";" << std::endl;
+}
+
+void IRSubFloat::print() {
+    std::cout << getResult()->getName() << " := "
+              << getArg1()->getName() << " - "
+              << getArg2()->getName() << ";" << std::endl;
+}
+
+void IRSubDouble::print() {
     std::cout << getResult()->getName() << " := "
               << getArg1()->getName() << " - "
               << getArg2()->getName() << ";" << std::endl;
@@ -166,13 +300,49 @@ void IRNegInt::print() {
               << getArg1()->getName() << ";" << std::endl;
 }
 
+void IRNegFloat::print() {
+    std::cout << getResult()->getName() << " := "
+              << " - "
+              << getArg1()->getName() << ";" << std::endl;
+}
+
+void IRNegDouble::print() {
+    std::cout << getResult()->getName() << " := "
+              << " - "
+              << getArg1()->getName() << ";" << std::endl;
+}
+
 void IRMulInt::print() {
     std::cout << getResult()->getName() << " := "
               << getArg1()->getName() << " * "
               << getArg2()->getName() << ";" << std::endl;
 }
 
+void IRMulFloat::print() {
+    std::cout << getResult()->getName() << " := "
+              << getArg1()->getName() << " * "
+              << getArg2()->getName() << ";" << std::endl;
+}
+
+void IRMulDouble::print() {
+    std::cout << getResult()->getName() << " := "
+              << getArg1()->getName() << " * "
+              << getArg2()->getName() << ";" << std::endl;
+}
+
 void IRDivInt::print() {
+    std::cout << getResult()->getName() << " := "
+              << getArg1()->getName() << " / "
+              << getArg2()->getName() << ";" << std::endl;
+}
+
+void IRDivFloat::print() {
+    std::cout << getResult()->getName() << " := "
+              << getArg1()->getName() << " / "
+              << getArg2()->getName() << ";" << std::endl;
+}
+
+void IRDivDouble::print() {
     std::cout << getResult()->getName() << " := "
               << getArg1()->getName() << " / "
               << getArg2()->getName() << ";" << std::endl;
@@ -190,28 +360,84 @@ void IRIfGreaterThanZeroGoto::print() {
               << "Goto " << getResult()->getName() << ";" << std::endl;
 }
 
-void IRIfGreaterThanGoto::print() {
+void IRIfGreaterThanGotoW::print() {
     std::cout << "if "
               << getArg1()->getName() << " > "
               << getArg2()->getName() 
               << "Goto " << getResult()->getName() << ";" << std::endl;
 }
 
-void IRIfGreaterEqualThanGoto::print() {
+void IRIfGreaterThanGotoF::print() {
+    std::cout << "if "
+              << getArg1()->getName() << " > "
+              << getArg2()->getName() 
+              << "Goto " << getResult()->getName() << ";" << std::endl;
+}
+
+void IRIfGreaterThanGotoD::print() {
+    std::cout << "if "
+              << getArg1()->getName() << " > "
+              << getArg2()->getName() 
+              << "Goto " << getResult()->getName() << ";" << std::endl;
+}
+
+void IRIfGreaterEqualThanGotoW::print() {
     std::cout << "if "
               << getArg1()->getName() << " >= "
               << getArg2()->getName() 
               << "Goto " << getResult()->getName() << ";" << std::endl;
 }
 
-void IRIfLessThanGoto::print() {
+void IRIfGreaterEqualThanGotoF::print() {
+    std::cout << "if "
+              << getArg1()->getName() << " >= "
+              << getArg2()->getName() 
+              << "Goto " << getResult()->getName() << ";" << std::endl;
+}
+
+void IRIfGreaterEqualThanGotoD::print() {
+    std::cout << "if "
+              << getArg1()->getName() << " >= "
+              << getArg2()->getName() 
+              << "Goto " << getResult()->getName() << ";" << std::endl;
+}
+
+void IRIfLessThanGotoW::print() {
     std::cout << "if "
               << getArg1()->getName() << " < "
               << getArg2()->getName() 
               << "Goto " << getResult()->getName() << ";" << std::endl;
 }
 
-void IRIfLessEqualThanGoto::print() {
+void IRIfLessThanGotoF::print() {
+    std::cout << "if "
+              << getArg1()->getName() << " < "
+              << getArg2()->getName() 
+              << "Goto " << getResult()->getName() << ";" << std::endl;
+}
+
+void IRIfLessThanGotoD::print() {
+    std::cout << "if "
+              << getArg1()->getName() << " < "
+              << getArg2()->getName() 
+              << "Goto " << getResult()->getName() << ";" << std::endl;
+}
+
+void IRIfLessEqualThanGotoW::print() {
+    std::cout << "if "
+              << getArg1()->getName() << " <= "
+              << getArg2()->getName() 
+              << "Goto " << getResult()->getName() << ";" << std::endl;
+}
+
+void IRIfLessEqualThanGotoF::print() {
+    std::cout << "if "
+              << getArg1()->getName() << " <= "
+              << getArg2()->getName() 
+              << "Goto " << getResult()->getName() << ";" << std::endl;
+}
+
+void IRIfLessEqualThanGotoD::print() {
     std::cout << "if "
               << getArg1()->getName() << " <= "
               << getArg2()->getName() 
--- src/IRCode.h
+++ src/IRCode.h
@@ -8,28 +8,53 @@
 #include "IROperand.h"
 
 enum IROperation {
-    PARAM,
+    PARAM_W,
+    PARAM_F,
+    PARAM_D,
     LABEL_HERE,
     CALL,
-    RETURN,
+    RETURN_W,
+    RETURN_F,
+    RETURN_D,
     COPY_W,
+    COPY_F,
     COPY_D,
     COPY_FROM_INDEXED_W,
+    COPY_FROM_INDEXED_F,
     COPY_FROM_INDEXED_D,
     COPY_TO_INDEXED_W,
+    COPY_TO_INDEXED_F,
     COPY_TO_INDEXED_D,
     ADD_INT,
+    ADD_FLOAT,
+    ADD_DOUBEL,
     SUB_INT,
+    SUB_FLOAT,
+    SUB_DOUBEL,
     NEG_INT,
+    NEG_FLOAT,
+    NEG_DOUBLE,
     MUL_INT,
+    MUL_FLOAT,
+    MUL_DOUBLE,
     DIV_INT,
+    DIV_FLOAT,
+    DIV_DOUBLE,
     MOD_INT,
 
     IF_GREATER_THAN_ZERO_GOTO,
-    IF_GREATER_THAN_GOTO,
-    IF_GREATER_EQUAL_THAN_GOTO,
-    IF_LESS_THAN_GOTO,
-    IF_LESS_EQUAL_THAN_GOTO,
+    IF_GREATER_THAN_GOTO_W,
+    IF_GREATER_THAN_GOTO_F,
+    IF_GREATER_THAN_GOTO_D,
+    IF_GREATER_EQUAL_THAN_GOTO_W,
+    IF_GREATER_EQUAL_THAN_GOTO_F,
+    IF_GREATER_EQUAL_THAN_GOTO_D,
+    IF_LESS_THAN_GOTO_W,
+    IF_LESS_THAN_GOTO_F,
+    IF_LESS_THAN_GOTO_D,
+    IF_LESS_EQUAL_THAN_GOTO_W,
+    IF_LESS_EQUAL_THAN_GOTO_F,
+    IF_LESS_EQUAL_THAN_GOTO_D,
 };
 
 class IRCode {
@@ -58,9 +83,24 @@ public:
     virtual void print();
 };
 
-class IRParam : public IRCode {
+class IRParamW : public IRCode {
 public:
-    IRParam(IROperand * new_arg1);
+    IRParamW(IROperand * new_arg1);
+
+    virtual void print();
+};
+
+
+class IRParamF : public IRCode {
+public:
+    IRParamF(IROperand * new_arg1);
+
+    virtual void print();
+};
+
+class IRParamD : public IRCode {
+public:
+    IRParamD(IROperand * new_arg1);
 
     virtual void print();
 };
@@ -73,9 +113,23 @@ public:
     virtual void print();
 };
 
-class IRReturn : public IRCode {
+class IRReturnW : public IRCode {
 public:
-    IRReturn(IROperand * new_arg1);
+    IRReturnW(IROperand * new_arg1);
+
+    virtual void print();
+};
+
+class IRReturnF : public IRCode {
+public:
+    IRReturnF(IROperand * new_arg1);
+
+    virtual void print();
+};
+
+class IRReturnD : public IRCode {
+public:
+    IRReturnD(IROperand * new_arg1);
 
     virtual void print();
 };
@@ -87,6 +141,13 @@ public:
 class IRCopyW : public IRCode {
 public:
     IRCopyW(IROperand * new_result, IROperand * new_arg1);
+
+    virtual void print();
+};
+
+class IRCopyF : public IRCode {
+public:
+    IRCopyF(IROperand * new_result, IROperand * new_arg1);
 
     virtual void print();
 };
@@ -105,6 +166,13 @@ public:
     virtual void print();
 };
 
+class IRCopyFromIndexedF : public IRCode {
+public:
+    IRCopyFromIndexedF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
 class IRCopyFromIndexedD : public IRCode {
 public:
     IRCopyFromIndexedD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
@@ -115,6 +183,13 @@ public:
 class IRCopyToIndexedW : public IRCode {
 public:
     IRCopyToIndexedW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRCopyToIndexedF : public IRCode {
+public:
+    IRCopyToIndexedF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
 
     virtual void print();
 };
@@ -133,9 +208,36 @@ public:
     virtual void print();
 };
 
+class IRAddFloat : public IRCode {
+public:
+    IRAddFloat(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRAddDouble : public IRCode {
+public:
+    IRAddDouble(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
 class IRSubInt : public IRCode {
 public:
     IRSubInt(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRSubFloat : public IRCode {
+public:
+    IRSubFloat(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRSubDouble : public IRCode {
+public:
+    IRSubDouble(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
 
     virtual void print();
 };
@@ -147,6 +249,20 @@ public:
     virtual void print();
 };
 
+class IRNegFloat : public IRCode {
+public:
+    IRNegFloat(IROperand * new_result, IROperand * new_arg1);
+
+    virtual void print();
+};
+
+class IRNegDouble : public IRCode {
+public:
+    IRNegDouble(IROperand * new_result, IROperand * new_arg1);
+
+    virtual void print();
+};
+
 class IRMulInt : public IRCode {
 public:
     IRMulInt(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
@@ -154,9 +270,37 @@ public:
     virtual void print();
 };
 
+class IRMulFloat : public IRCode {
+public:
+    IRMulFloat(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRMulDouble : public IRCode {
+public:
+    IRMulDouble(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
 class IRDivInt : public IRCode {
 public:
     IRDivInt(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRDivFloat : public IRCode {
+public:
+    IRDivFloat(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRDivDouble : public IRCode {
+public:
+    IRDivDouble(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
 
     virtual void print();
 };
@@ -175,30 +319,86 @@ public:
     virtual void print();
 };
 
-class IRIfGreaterThanGoto : public IRCode {
+class IRIfGreaterThanGotoW : public IRCode {
 public:
-    IRIfGreaterThanGoto(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+    IRIfGreaterThanGotoW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
 
     virtual void print();
 };
 
-class IRIfGreaterEqualThanGoto : public IRCode {
+class IRIfGreaterThanGotoF : public IRCode {
 public:
-    IRIfGreaterEqualThanGoto(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+    IRIfGreaterThanGotoF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
 
     virtual void print();
 };
 
-class IRIfLessThanGoto : public IRCode {
+class IRIfGreaterThanGotoD : public IRCode {
 public:
-    IRIfLessThanGoto(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+    IRIfGreaterThanGotoD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
 
     virtual void print();
 };
 
-class IRIfLessEqualThanGoto : public IRCode {
+class IRIfGreaterEqualThanGotoW : public IRCode {
 public:
-    IRIfLessEqualThanGoto(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+    IRIfGreaterEqualThanGotoW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRIfGreaterEqualThanGotoF : public IRCode {
+public:
+    IRIfGreaterEqualThanGotoF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRIfGreaterEqualThanGotoD : public IRCode {
+public:
+    IRIfGreaterEqualThanGotoD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRIfLessThanGotoW : public IRCode {
+public:
+    IRIfLessThanGotoW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRIfLessThanGotoF : public IRCode {
+public:
+    IRIfLessThanGotoF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRIfLessThanGotoD : public IRCode {
+public:
+    IRIfLessThanGotoD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRIfLessEqualThanGotoW : public IRCode {
+public:
+    IRIfLessEqualThanGotoW(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRIfLessEqualThanGotoF : public IRCode {
+public:
+    IRIfLessEqualThanGotoF(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
+
+    virtual void print();
+};
+
+class IRIfLessEqualThanGotoD : public IRCode {
+public:
+    IRIfLessEqualThanGotoD(IROperand * new_result, IROperand * new_arg1, IROperand * new_arg2);
 
     virtual void print();
 };
